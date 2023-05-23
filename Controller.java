185b46cc17a8c765af2d239cba617ec2bbb0802e
Asmaa <101478736+Asmaabnh@users.noreply.github.com>
2023-05-23 02:48:01 +0200

    mod

--- Controller.java
+++ Controller.java
@@ -1,30 +1,80 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 
 
 public class Controller {
 
-    @FXML
-    private Button signUpbutton;
 
     @FXML
-    void signUp(ActionEvent event) { //INSCRIRE 
+    private Button INSCRIPTION;
+
+    @FXML
+    private Button CONNEXION;
+
+
+    @FXML
+    private TextArea pseudo;
+
+    @FXML
+    private ComboBox<?> nbtachemin;
+
+
+
+    @FXML
+    private Button inscrire;
+
+   
+   
+
+    @FXML
+    void inscription(ActionEvent event) {
+
+      //INSCRIRE 
         Authgestion gestion = new Authgestion() ; 
         //SERIALISATION 
         //INSCIRE() 
-        
-        gestion.Inscrire(null);
-        Parent root = FXMLLoader.load(getClass().getResource("PERIODE.fxml"));
+        INSCRIPTION.setOnAction(e -> {
+        Parent root = FXMLLoader.load(getClass().getResource("inscription.fxml"));
         Scene scene = new Scene(root);
         Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+
+        String nom = pseudo.getText();
+        System.out.println("Contenu du TextArea : " + nom);
+
+       
+
+         
+
+        Utilisateur user = new Utilisateur(null, 0);
+        gestion.Inscrire(null);
+
+
+        });
+        
+       
+        
+    }
+
+    @FXML
+    void connexion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void periode(ActionEvent event) {
+
     }
 
 
@@ -34,14 +84,8 @@ public class Controller {
         Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
-    }
-    public void handleInscriptionq(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
-        Scene scene = new Scene(root);
-        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.show();
-    } */
+    }*/
+  
 
 }
 
